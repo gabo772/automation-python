bb8bbd49ea8002c1533ace65847a92ea70ae7e97
gabriel <gabriel.ramirez.lemus@gmail.com>
2025-05-25 01:50:24 -0400

    se agregan options para correr en jenkins

--- features/environment.py
+++ features/environment.py
@@ -28,15 +28,19 @@ def before_scenario(context,scenario):
     if tipo_ejecucion=="grid":
         grid_url=os.getenv("GRID_URL")
         context.browser= webdriver.Remote(command_executor=grid_url,options=navegador)
+    elif tipo_ejecucion=="jenkins":
+        navegador.add_argument('--headless')
+        navegador.add_argument('--no-sandbox')
+        navegador.add_argument('--disable-dev-shm-usage')
     else:
         if "firefox" in scenario.tags:
-            context.browser = webdriver.Firefox()
+            context.browser = webdriver.Firefox(options=navegador)
         elif "edge" in scenario.tags:
-            context.browser = webdriver.Edge()
+            context.browser = webdriver.Edge(options=navegador)
         elif "safari" in scenario.tags:
-            context.browser = webdriver.Safari()
+            context.browser = webdriver.Safari(options=navegador)
         else:
-            context.browser = webdriver.Chrome()
+            context.browser = webdriver.Chrome(options=navegador)
     context.browser.implicitly_wait(5)
     context.browser.maximize_window()
 
